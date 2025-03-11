e950ee2aa2170a3385798170f7b4aac67f5c035e
ttetricas <estermeireles89@gmail.com>
2025-03-11 13:10:39 -0300

    cálculo montante concluído

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿decimal c = 1200m, i = 2m;// capital inicial e taxa de juros 
+int t = 15;// tempo 
+
+decimal j = (c * i * t) / 100;
+decimal m = c + j;
+
+Console.WriteLine($"Juros: R$ {j} \nMontante: R$ {m}");
